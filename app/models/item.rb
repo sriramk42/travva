c31c2741960efb1e82d62c787352a0f9e3149446
Sriram Krishnan <sriramk1986@gmail.com>
2019-11-19 12:28:44 +0900

    Added details for cloudinary

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   has_many :trip_items, dependent: :destroy
   validates :country, presence: true
   validates :title, presence: true
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: {only_integer: true}
-  validates :price, numericality: {only_float: true}
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
+  validates :price, numericality: { only_float: true }
+  mount_uploader :photo, PhotoUploader
 end
