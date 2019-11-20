4ab8bba26443559a79ffd806e8aeb6294b49930f
Sriram Krishnan <sriramk1986@gmail.com>
2019-11-20 14:59:33 +0900

    Added user firstname, lastname and avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :photo, PhotoUploader
 
   has_many :items, dependent: :destroy
   has_many :trips, dependent: :destroy
