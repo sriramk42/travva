abb66ef452d2d029688f7bea4e38a8556a9ddb7c
Sriram Krishnan <sriramk1986@gmail.com>
2019-11-20 13:50:21 +0900

    Added missing comma in package.json

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_20_014009) do
 
   # These are extensions that must be enabled in order to support this database
@@ -30,9 +29,9 @@ ActiveRecord::Schema.define(version: 2019_11_20_014009) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.float "latitude"
     t.float "longitude"
-    t.string "photo"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
