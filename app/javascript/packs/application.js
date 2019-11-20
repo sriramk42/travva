33ac6d8fbc52749392da2484b6d9e46b883457ff
Sriram Krishnan <sriramk1986@gmail.com>
2019-11-20 18:19:41 +0900

    Added algolia places

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,12 @@
 import "bootstrap";
 
-
 // app/javascript/packs/application.js
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import "mapbox-gl/dist/mapbox-gl.css"; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
-import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from "plugins/init_autocomplete";
+import { initMapbox } from "../plugins/init_mapbox";
 
+initAutocomplete();
 initMapbox();
 
-import { home } from '../home/home'
+import { home } from "../home/home";
