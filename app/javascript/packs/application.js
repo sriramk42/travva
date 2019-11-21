9f284e3cb04defd0109a550e0cab9884a206be75
Sriram Krishnan <sriramk1986@gmail.com>
2019-11-21 22:32:55 +0900

    Small JS fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,29 +1,10 @@
 import "bootstrap";
-
-// app/javascript/packs/application.js
 import "mapbox-gl/dist/mapbox-gl.css"; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-
+import "plugins/flatpickr";
 import { initAutocomplete } from "plugins/init_autocomplete";
 import { initMapbox } from "../plugins/init_mapbox";
+import { home } from "../home/home";
+import { initSweetalert } from "../plugins/init_sweetalert";
 
 initAutocomplete();
 initMapbox();
-
-import { home } from "../home/home";
-
-import { initSweetalert } from "../plugins/init_sweetalert";
-
-
-import 'plugins/flatpickr'
-
-// initSweetalert('#sweet-alert-demo', {
-//   title: "Nice!",
-//   text: "Item successfully created",
-//   icon: "success"
-// }, (value) => {
-//   if (value) {
-//     const link = document.querySelector('#submit-link');
-//     link.click();
-//   }
-// });
-
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,8 @@
-import flatpickr from 'flatpickr'
-import 'flatpickr/dist/flatpickr.min.css'
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 flatpickr("#start_date", {
   altInput: true,
-  "plugins": [new rangePlugin({ input: "#end_date"})]
+  plugins: [new rangePlugin({ input: "#end_date" })]
 });
