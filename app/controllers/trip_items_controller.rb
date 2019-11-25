673ca1b72e16176aa9c802dddcadc0491a0e2a7a
Silvia Rocha <silviadprocha@gmail.com>
2019-11-25 18:14:35 +0900

    add trip button to card

--- app/controllers/trip_items_controller.rb
+++ app/controllers/trip_items_controller.rb
@@ -2,7 +2,8 @@ class TripItemsController < ApplicationController
   def create
     @trip_item = TripItem.new
     @trip_item.trip_id = params[:trip_id]
-    @trip_item.item_id = params[:item]
+    @item_id = params[:item]
+    @trip_item.item_id = @item_id
     authorize @trip_item
     if @trip_item.save
       flash.now[:notice] = 'Item correctly added to your trip'
