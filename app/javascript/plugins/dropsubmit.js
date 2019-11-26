4e353ef93e52da3ecc201e713f88eca16cad2a87
Sabrina Gheissari <sabrina.gheissari1@gmail.com>
2019-11-26 14:07:12 +0900

    button functioning

--- app/javascript/plugins/dropsubmit.js
+++ app/javascript/plugins/dropsubmit.js
@@ -1,5 +1,6 @@
 const dropdownSubmit = () => {
   const forms = document.querySelectorAll('.dropdown-form');
+  console.log(forms);
   forms.forEach((form) => {
     const dropdown = form.querySelector('select');
     dropdown.addEventListener('change', (event) => {
