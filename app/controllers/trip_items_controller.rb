a895c67fc2105e5be335732c1b7f44ae98c47f8a
Sabrina Gheissari <sabrina.gheissari1@gmail.com>
2019-11-26 10:23:46 +0900

    adding date button to items for trip show page

--- app/controllers/trip_items_controller.rb
+++ app/controllers/trip_items_controller.rb
@@ -1,11 +1,26 @@
 class TripItemsController < ApplicationController
   def create
+    if params[:date]
+      @trip_item.date = params[:date]
+              redirect_to trip_path(@trip_item.trip_id)
+
+    end
+
     @trip_item = TripItem.new
     @trip_item.trip_id = params[:trip_id]
     @item_id = params[:item]
     @trip_item.item_id = @item_id
+
+    if params[:date]
+      @trip_item.date = params[:date]
+    end
+
     authorize @trip_item
+    raise
     if @trip_item.save
+      if params[:date]
+        redirect_to trip_path(@trip_item.trip_id)
+      end
       flash.now[:notice] = 'Item correctly added to your trip'
     else
       flash.now[:alert] = 'You have already added this item to your trip'
